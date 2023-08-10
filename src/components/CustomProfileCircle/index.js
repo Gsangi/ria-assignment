0d4d9c53136dced44412dda7fa76a372b4390682
Gsangi <sanskar6053@gmail.com>
2023-08-10 10:36:39 +0530

    refactor: changed textstyle and fontsizes of text

--- src/components/CustomProfileCircle/index.js
+++ src/components/CustomProfileCircle/index.js
@@ -1,17 +1,14 @@
 import {Box, Circle, Flex, Text} from "@chakra-ui/react";
 
-const CustomProfileCircle = ({number, text}) => {
+const CustomProfileCircle = ({text}) => {
     return (
         <Flex direction="column" justifyContent="center" alignContent="center" id = "CustomProfileCircle">
-            {/*<Circle size="38px" border="3px" my="6px" borderColor="black">*/}
-            {/*    {number}*/}
-            {/*</Circle>*/}
             <Circle size='40px' border="4px" borderColor="black" color='white'>
-                    <Box as='span' fontWeight='bold' fontSize='lg' color="black">
-                        1
+                    <Box as='span' fontSize='15px' color="black" textStyle="circleProfileText">
+                        499
                     </Box>
             </Circle>
-           <Text textStyle="circleProfileText">{text}</Text>
+           <Text textStyle="circleProfileText" my="6px">{text}</Text>
         </Flex>
     )
 }
