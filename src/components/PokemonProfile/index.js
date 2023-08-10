8f82b2eb5254d2f06a6cc397537651bf3335b6e7
Gsangi <sanskar6053@gmail.com>
2023-08-10 10:38:11 +0530

    refactor: Changed css properties and added animation for the PokemonDirectory

--- src/components/PokemonProfile/index.js
+++ src/components/PokemonProfile/index.js
@@ -1,18 +1,19 @@
 import { Flex, Text } from "@chakra-ui/react";
 import CustomProfileCircle from "../CustomProfileCircle";
+import { DragonPikachu } from "../Icons/dragonPikachu";
 
-const PokemonProfile = ({ text, number, circleText, onClick }) => {
+const PokemonProfile = ({ text, number, onClick }) => {
     return (
-        <Flex height="136px" width="352px" onClick={onClick}>
-            <Flex display="column">
-                <Text my="6px" textStyle="heading3">{text}</Text>
-                <Flex direction="row">
-                    <CustomProfileCircle number={number} text={circleText}/>
-                    <CustomProfileCircle number={number} text={circleText}/>
+        <Flex height="136px" width="352px" onClick={onClick} ml="17px" my="23px" bg="rgba(246, 247, 249, 1)" borderRadius="8px">
+            <Flex display="column" flex="0.75">
+                <Text mt="6px" mb="12px" textStyle="heading3" textAlign="center">{text}</Text>
+                <Flex direction="row" mt="6px" gap="18px" mr="2px" justifyContent="end">
+                    <CustomProfileCircle number={number} text="Attack"/>
+                    <CustomProfileCircle number={number} text="Defense"/>
                 </Flex>
             </Flex>
-            <Flex>
-                Image
+            <Flex bg="custom.danger" flex="1.25" justifyContent="center" alignItems="center" borderTopRightRadius="8px" borderBottomRightRadius="8px">
+                <DragonPikachu />
             </Flex>
         </Flex>
     )
--- src/containers/PokemonDirectory/index.js
+++ src/containers/PokemonDirectory/index.js
@@ -1,8 +1,17 @@
-import {Flex, Input, Text, Box, Button, useDisclosure} from "@chakra-ui/react";
+import { Flex, Input, Text, useDisclosure, Circle } from "@chakra-ui/react";
+import { motion, AnimatePresence } from "framer-motion";
 import PokemonProfile from "../../components/PokemonProfile";
 import usePokemonApi from "../../hooks/usePokemonApi";
 import { useState } from "react";
 import ProfileModal from "../../components/ProfileModal";
+
+const MotionFlex = motion(Flex);
+
+const pageTransitionVariants = {
+    hidden: { opacity: 0, y: "-10%" },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: "10%" }
+};
 
 const PokemonDirectory = () => {
     const { data: pokemonList, isLoading } = usePokemonApi();
@@ -43,43 +52,59 @@ const PokemonDirectory = () => {
                 800 Pokemons for you to choose your favorite
             </Text>
             <Input
-                placeholder="medium size"
+                placeholder="Enter Pokémon name"
                 size="md"
-                marginLeft="176px"
-                marginRight="176px"
                 value={searchTerm}
                 onChange={(event) => setSearchTerm(event.target.value)}
                 mt="17px"
                 mb="25px"
                 width="1088px"
             />
-            <Flex wrap="wrap" justify="center" mx="157px">
-                {currentItems.map((pokemon) => {
-                    return (
+            <AnimatePresence mode = 'wait'>
+                <MotionFlex
+                    wrap="wrap"
+                    justify="center"
+                    mx="157px"
+                    key={currentPage}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={pageTransitionVariants}
+                >
+                    {currentItems.map((pokemon) => (
                         <PokemonProfile
                             key={pokemon.number}
                             text={pokemon.name}
                             number={pokemon.number}
                             circleText={pokemon.circleText}
-                            onClick={() => openModal(pokemon)}  // New onClick prop
+                            onClick={() => openModal(pokemon)}
                         />
-                    );
-                })}
-                {selectedPokemon && (
-                    <ProfileModal
-                        isOpen={isOpen}
-                        onClose={onClose}
-                        pokemon={selectedPokemon}
-                    />
-                )}
-            </Flex>
-            <Box>
+                    ))}
+                    {selectedPokemon && (
+                        <ProfileModal
+                            isOpen={isOpen}
+                            onClose={onClose}
+                            pokemon={selectedPokemon}
+                        />
+                    )}
+                </MotionFlex>
+            </AnimatePresence>
+
+            {/* Pagination dots */}
+            <Flex direction="row" alignItems="center" justifyContent="center" mt="16px">
                 {pageNumbers.map((number) => (
-                    <Button onClick={() => paginate(number)} key={number}>
-                        {number}
-                    </Button>
+                    <Circle
+                        key={number}
+                        size="8px"
+                        m="2px"
+                        bgColor={currentPage === number ? "black" : "gray.400"}
+                        cursor="pointer"
+                        transition={{ duration: 0.3 }}
+                        _hover={{ bgColor: "black" }}
+                        onClick={() => paginate(number)}
+                    />
                 ))}
-            </Box>
+            </Flex>
         </Flex>
     );
 };
